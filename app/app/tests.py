2821b78f4f3b399e28c01ff9cf1f139bec3d2c9d
masterinapi <chadortenzin13545@gmail.com>
2023-02-04 16:29:31 +0600

    'Changes'

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CaclTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
